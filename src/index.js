fa3c0f6f5131aa79734515b5c155a84d4dd633bb
vadim_yakovenko <vadim_yakovenko@struktura.io>
2025-12-11 18:14:43 +0200

    inst

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 import './styles.css';
 
-(function (w, d) {
-    w.PushEngage = w.PushEngage || [];
-    w._peq = w._peq || [];
-    PushEngage.push(['init', {
-        appId: 'c2d99a3c-a5a2-432e-a417-d8e8b2015525'
-    }]);
-
-    var e = d.createElement('script');
-
-    e.src = 'https://clientcdn.pushengage.com/sdks/pushengage-web-sdk.js';
-    e.async = true;
-    e.type = 'text/javascript';
-    d.head.appendChild(e);
-})(window, document);
-
-
 window.addEventListener('DOMContentLoaded', () => {
     console.log('DOMContentLoaded');
 
